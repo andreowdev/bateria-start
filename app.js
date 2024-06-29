55c889a10a7665eaffe5d6e6325736149dbacaec
Andreo Henrique <129970769+andreowdev@users.noreply.github.com>
2024-06-29 00:17:08 -0300

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ document.addEventListener('keydown', function(event) {
 function makeSounds(key) {
 
       switch (key) {
-        case 'w':
+        case 'enter':
               let tom1 = new Audio('./sounds/tom-1.mp3');
             tom1.play();
             break;
